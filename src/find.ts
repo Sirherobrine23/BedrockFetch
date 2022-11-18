17972dea3db5f9c64b3089568f2dc84c019ead87
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-11-18 00:23:54 +0000

    Update files

--- src/find.ts
+++ src/find.ts
@@ -1,8 +1,9 @@
-import { httpRequest } from "@the-bds-maneger/core-utils";
+import { httpRequest, httpRequestLarge } from "@the-bds-maneger/core-utils";
 
 export type bedrockSchema = {
   version: string,
   date: Date,
+  release?: "stable"|"preview",
   url: {
     [platform in NodeJS.Platform]?: {
       [arch in NodeJS.Architecture]?: string
@@ -10,13 +11,12 @@ export type bedrockSchema = {
   }
 };
 
-find().then(console.log).catch(err => console.log(err?.response.body?.toString()||err));
 export async function find() {
-  const minecraftUrls = (await httpRequest.urls("https://www.minecraft.net/en-us/download/server/bedrock")).filter(Link => /bin-.*\.zip/.test(Link) && !(/preview/).test(Link));
+  const minecraftUrls = (await httpRequest.urls("https://www.minecraft.net/en-us/download/server/bedrock")).filter(Link => /bin-.*\.zip/.test(Link));
   const objURLs = minecraftUrls.reduce((mount, url) => {
     // get version
     let mcpeVersion = url;
-    const regReplace = /((^http[s]:\/\/[a-z\.]+\/bin-[a-zA-Z0-9]+\/)|(\.zip$)|([a-zA-Z\-]+))/;
+    const regReplace = /((^http[s]:\/\/[a-z\.]+\/bin-[a-zA-Z0-9\-]+\/)|(\.zip$)|([a-zA-Z\-]+))/;
     while (regReplace.test(mcpeVersion)) mcpeVersion = mcpeVersion.replace(regReplace, "");
     if (!mcpeVersion) return mount;
     if (!mount[mcpeVersion]) mount[mcpeVersion] = {};
@@ -35,6 +35,16 @@ export async function find() {
     }
     return mount;
   }, {} as {version: bedrockSchema["url"]});
-
-  return objURLs;
+  const data: bedrockSchema[] = [];
+  for (const version of Object.keys(objURLs)) {
+    const versionData = objURLs[version];
+    const fistUrl = versionData[Object.keys(versionData).at(0)][Object.keys(versionData[Object.keys(versionData).at(0)]).at(0)];
+    data.push({
+      version: version,
+      date: (((await httpRequestLarge.zipDownload(fistUrl)).getEntries()).find(file => file.entryName.startsWith("bedrock_server")))?.header?.time||new Date(),
+      release: ((/preview/).test(fistUrl))?"preview":"stable",
+      url: versionData,
+    });
+  }
+  return data;
 }
